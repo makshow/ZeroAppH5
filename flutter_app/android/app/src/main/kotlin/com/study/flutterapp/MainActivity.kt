433ec086fcac6a197bc6e8491ade02e2c8785ae4
tock <2966497308qq.com>
2020-06-21 20:28:11 +0800

    flutter_new新项目

--- flutter_app/android/app/src/main/kotlin/com/study/flutterapp/MainActivity.kt
+++
@@ -1,9 +0,0 @@
-package com.study.flutterapp
-/*  解决android模拟器无法链接网络
-    cd /Users/smellzero/Library/Android/sdk/emulator
-     ./emulator -avd Pixel_3_API_28 -dns-server 8.8.8.8,114.114.114.114
- */
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
